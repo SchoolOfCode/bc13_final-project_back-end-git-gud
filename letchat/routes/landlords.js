e993089d0c79d3b088b2b3ab57d22a0fb8ddfa16
cowtipping <turbogerbil@gmail.com>
2023-01-22 16:46:16 +0000

    comments and tidy-up

--- letchat/routes/landlords.js
+++ letchat/routes/landlords.js
@@ -3,14 +3,13 @@ import { getAllLandlords } from "../models/landlords.js";
 
 export const landlordsRouter = express.Router();
 
+// Get all landlords
 landlordsRouter.get("/", async (req, res) => {
   try {
     const allLandlords = await getAllLandlords();
-    console.log(allLandlords);
     res.status(200);
     res.json({ success: true, payload: allLandlords });
   } catch (error) {
-    console.log(error);
     res.status(500);
     res.json({ success: false, message: "Failed to get all landlords" });
   }

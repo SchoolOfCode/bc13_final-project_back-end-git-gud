5f04b384d0f3c5bb0f18261705e19a434b1323d6
cowtipping <turbogerbil@gmail.com>
2023-01-31 19:37:27 +0000

    try/catch blocks

--- letchat/models/landlords.js
+++ letchat/models/landlords.js
@@ -1,13 +1,24 @@
 import pool from "../db/index.js";
 
 export async function getAllLandlords() {
-  const query = "SELECT * FROM landlords";
-  const result = await pool.query(query);
-  return result.rows;
+  try {
+    const query = "SELECT * FROM landlords";
+    const result = await pool.query(query);
+    return result.rows;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
 
 export async function getAllPropertiesByLandlordID(landlord_id) {
-  const query = "SELECT properties.address, properties.postcode, properties.landlord_id FROM properties WHERE landlord_id = $1";
-  const result = await pool.query(query, [landlord_id]);
-  return result.rows;
+  try {
+    const query =
+      "SELECT properties.address, properties.postcode, properties.landlord_id FROM properties WHERE landlord_id = $1";
+    const result = await pool.query(query, [landlord_id]);
+    return result.rows;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
